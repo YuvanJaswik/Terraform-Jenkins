590a92f0c184ae89bccf32abb0722537bcc16f3a
YuvanJaswik <arepallyrajavardhanreddy@gmail.com>
2024-09-18 20:36:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_instance" "ExampleAppServerInstance" {
   ami           = "ami-05fa00d4c63e32376"
   instance_type = "t2.micro"
 
